8f713b0465bd836333faac753865af57619e83b6
Walter <bookenghua@outlook.com>
2022-02-06 14:57:36 +0800

    basic tests (import error)

---
+++ lin_reg/model/test_data_store.py
@@ -0,0 +1,45 @@
+from data_store import DataStore
+import math
+import pytest
+
+def test_split():
+
+    test_x = [i + 2 for i in range(10)]
+    test_y = [i - 1 for i in range(10)]
+    n_folds = 3
+    raw_data = DataStore(test_x, test_y, n_folds)
+    raw_data.split()
+
+    folds_idx = raw_data.folds_idx
+    assert(len(folds_idx) == n_folds)
+    for fold in folds_idx:
+        assert(len(fold) - len(test_x) // n_folds >= 0)
+        assert(len(fold) - len(test_x) // n_folds <= 1)
+
+def test_data_store_exception():
+    # Test 1: x_raw, y_raw unequal length
+    test_x = [i + 2 for i in range(7)]
+    test_y = [i - 1 for i in range(10)]
+    n_folds = 3
+
+    with pytest.raises(ValueError) as excinfo:
+        raw_data = DataStore(test_x, test_y, n_folds)
+        assert "Unequal X and Y Data Points" in excinfo.value
+
+    #Test 2: number of folds exceed number of data points
+    test_x = [i - 1 for i in range(10)]
+    test_y = [i + 1 for i in range(10)]
+    n_folds = 13
+
+    with pytest.raises(ValueError) as excinfo:
+        raw_data = DataStore(test_x, test_y, n_folds)
+        assert "Number of Folds exceed Number of Data Points" in excinfo.value
+
+    #Test 3: need at least 2 folds for validation
+    test_x = [i + 2 for i in range(10)]
+    test_y = [i - 1 for i in range(10)]
+    n_folds = 1
+
+    with pytest.raises(ValueError) as excinfo:
+        raw_data = DataStore(test_x, test_y, n_folds)
+        assert "Need at least 2 folds for validation." in excinfo.value
---
+++ lin_reg/model/test_gaussian_kernel.py
@@ -0,0 +1,7 @@
+from data_store import DataStore
+from gaussian_kernel import GaussianKernel
+import math
+import pytest
+
+def test_find_total_weight():
+    is_float(total_weight)
