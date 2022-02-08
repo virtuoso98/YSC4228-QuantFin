8f56868069216a188cff6525d2a1a6ca0bfc9478
Walter <bookenghua@outlook.com>
2022-02-08 23:06:19 +0800

    finished most test

--- lin_reg/model/test_data_store.py
+++ lin_reg/model/test_data_store.py
@@ -1,5 +1,5 @@
-from data_store import DataStore
-import math, pytest
+from model.data_store import DataStore
+import pytest
 from random import random, randint, sample
 
 def test_split():
@@ -19,33 +19,36 @@ def test_split():
 
 def test_data_store_exception_unequal_length():
     # Test: x_raw, y_raw unequal length
-    rand_index = sample(range(2, 1000), 2)
-    n_folds = randint(2, min(rand_index))
-    test_x = [random() for _ in range(rand_index[0])]
-    test_y = [random() for _ in range(rand_index[1])]
+    for _ in range(100):
+        rand_index = sample(range(2, 1000), 2)
+        n_folds = randint(2, min(rand_index))
+        test_x = [random() for _ in range(rand_index[0])]
+        test_y = [random() for _ in range(rand_index[1])]
 
-    with pytest.raises(ValueError) as excinfo:
-        _ = DataStore(test_x, test_y, n_folds)
-        assert "Unequal X and Y Data Points" in excinfo.value
+        with pytest.raises(ValueError) as excinfo:
+            _ = DataStore(test_x, test_y, n_folds)
+            assert "Unequal X and Y Data Points" in excinfo.value
 
 def test_data_store_exception_excessive_folds():
     #Test: number of folds exceed number of data points
-    n_data = randint(2, 1000)
-    n_folds = randint(n_data + 1, n_data + 1000)
-    test_x = [random() for _ in range(n_data)]
-    test_y = [random() for _ in range(n_data)]
+    for _ in range(100):
+        n_data = randint(2, 1000)
+        n_folds = randint(n_data + 1, n_data + 1000)
+        test_x = [random() for _ in range(n_data)]
+        test_y = [random() for _ in range(n_data)]
 
-    with pytest.raises(ValueError) as excinfo:
-        raw_data = DataStore(test_x, test_y, n_folds)
-        assert "Number of Folds exceed Number of Data Points" in excinfo.value
+        with pytest.raises(ValueError) as excinfo:
+            raw_data = DataStore(test_x, test_y, n_folds)
+            assert "Number of Folds exceed Number of Data Points" in excinfo.value
 
 def test_data_store_exception_insufficient_folds():
     #Test: need at least 2 folds for validation
-    n_data = randint(2, 1000)
-    n_folds = randint(0, 1)
-    test_x = [random() for _ in range(n_data)]
-    test_y = [random() for _ in range(n_data)]
+    for _ in range(100):
+        n_data = randint(2, 1000)
+        n_folds = randint(0, 1)
+        test_x = [random() for _ in range(n_data)]
+        test_y = [random() for _ in range(n_data)]
 
-    with pytest.raises(ValueError) as excinfo:
-        raw_data = DataStore(test_x, test_y, n_folds)
-        assert "Need at least 2 folds for validation." in excinfo.value
+        with pytest.raises(ValueError) as excinfo:
+            raw_data = DataStore(test_x, test_y, n_folds)
+            assert "Need at least 2 folds for validation." in excinfo.value
--- lin_reg/model/test_gaussian_kernel.py
+++ lin_reg/model/test_gaussian_kernel.py
@@ -1,16 +1,70 @@
-from gaussian_kernel import GaussianKernel
-import pytest
+from model.gaussian_kernel import GaussianKernel
+from random import uniform, randint, random
+import pytest, math
 
-def test_split():
+def test_calc_kernel():
+    # Init kernel with dummy variables so exception
+    # does not occur. However, calc_kernel is static,
+    # so dummy values don't matter
+    dummy_data = [0, 0]
+    dummy_folds = 2
+    raw_data = GaussianKernel(dummy_data, dummy_data, dummy_folds)
 
-    test_x = [i + 2 for i in range(10)]
-    test_y = [i - 1 for i in range(10)]
-    n_folds = 3
-    raw_data = GaussianKernel(test_x, test_y, n_folds)
-    raw_data.split()
+    for _ in range(100):
+        test_x1 = uniform(-1000, 1000)
+        test_x2 = uniform(-1000, 1000)
+        test_h = uniform(0.1, 3)
+        
+        # Actual Value
+        actual_kernel = raw_data._calc_kernel(test_x1, test_x2, test_h)
 
-    folds_idx = raw_data.folds_idx
-    assert(len(folds_idx) == n_folds)
-    for fold in folds_idx:
-        assert(len(fold) - len(test_x) // n_folds >= 0)
-        assert(len(fold) - len(test_x) // n_folds <= 1)
+        # Expected value
+        x_delta = (test_x1 - test_x2) ** 2
+        expected_kernel = math.exp(- x_delta / test_h)
+
+        # Testing that they are equivalent, modulo floating pt error
+        assert abs(expected_kernel - actual_kernel) < 10 ** -6
+
+def test_find_total_weight():
+    # Populate test x to evaluate
+    test_x_ev = uniform(-10, 10)
+    test_h = uniform(0.1, 3)
+    # Populate test data
+    n_data = randint(2, 1000)
+    n_folds = randint(2, n_data)
+    test_x_train = [uniform(-10, 10) for _ in range(n_data)]
+    # Y dummy as it's not important to the test
+    dummy_y = [0 for _ in range(n_data)]
+    raw_data = GaussianKernel(test_x_train, dummy_y, n_folds)
+    # Can only test find_total_weight by populating x_train directly
+    raw_data.x_train = test_x_train
+
+    #Actual value
+    actual_weight = raw_data._find_total_weight(test_x_ev, test_h)
+
+    #Expected value
+    expected_weight = 0
+    for x_tr in raw_data.x_train:
+        x_delta = (test_x_ev - x_tr) ** 2
+        expected_weight += math.exp(- x_delta / test_h)
+
+    # Testing that they are equivalent, modulo floating pt error
+    assert abs(expected_weight - actual_weight) < 10 ** -6
+
+def test_get_y_pred():
+    # Populate test x to evaluate
+    test_x_ev = uniform(-10, 10)
+    test_h = uniform(0.1, 3)
+    # Populate test data
+    n_data = randint(2, 1000)
+    n_folds = randint(2, n_data)
+    test_x_train = [uniform(-10, 10) for _ in range(n_data)]
+    # Y dummy as it's not important to the test
+    dummy_y = [0 for _ in range(n_data)]
+    raw_data = GaussianKernel(test_x_train, dummy_y, n_folds)
+    # Can only test find_total_weight by populating x_train directly
+    raw_data.x_train = test_x_train
+
+    
+
+    
